7983d9c6cb5897a4fc0164cce374507e60e8b22f
hangyang.wang <w.why@163.com>
2016-01-29 17:02:53 +0800

    up

--- routes/operation.js
+++ routes/operation.js
@@ -293,7 +293,7 @@ if (!daili) {
         if (pageNo) {
             fileName = req.path + ':' + pageNo;
             if (fileapi[fileName]) {
-                console.log(fileName, ' replace as ', fileapi[fileName]);
+                //console.log(fileName, ' replace as ', fileapi[fileName]);
                 fs.readFile(path.resolve(__dirname, '../temp_data', fileapi[fileName]), {
                     encoding: 'utf-8'
                 }, function(err, data) {
@@ -309,7 +309,7 @@ if (!daili) {
             }
         } else {
             if (fileapi[req.path]) {
-                console.log(req.path, ' replace as ', fileapi[req.path]);
+                //console.log(req.path, ' replace as ', fileapi[req.path]);
                 fs.readFile(path.resolve(__dirname, '../temp_data', fileapi[req.path]), {
                     encoding: 'utf-8'
                 }, function(err, data) {
